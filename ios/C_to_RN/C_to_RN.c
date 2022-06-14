4326210890bf5b726b7cdab4eb87e396a6599e1b
Wynne-Tran <wynnetran2018@gmail.com>
2022-06-13 21:37:11 -0400

    ObjC to React Native

--- ios/C_to_RN/C_to_RN.c
+++
@@ -1,6 +0,0 @@
-#include "C_to_RN.h"
-
-long factorial(int n) {
-  if (n == 0 || n == 1) return 1;
-  return n * factorial(n-1);
-}
--- ios/C_to_RN/NativeModules-Bridging-Header.h
+++
@@ -1,2 +0,0 @@
-#import <React/RCTBridgeModule.h>
-#import "C_to_RN.h"
